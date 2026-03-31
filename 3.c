56e5ece4170b7cd1e9c4db47908fe7db04ae66fd
vashu-yadav <Vashu.26940@stu.upes.ac.in>
2026-03-31 23:02:00 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, flag = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1)
+        flag = 1;
+
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("Prime Number");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
